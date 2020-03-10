8f7f38daeee344cef01a07b09959aaddf6b5e717
Viacheslav Rudenko <slavezzz@inbox.ru>
2020-03-10 13:12:00 +0300

    Создали класс Point

---
+++ snake/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace snake
+{
+    class Point
+    {      public int x;
+           public int y;
+           public char sym;
+
+           public void Draw()
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(sym);
+            }
+    }
+}
+
