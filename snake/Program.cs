936032d9b7802f8c189bd75970d256fe9012c65f
Viacheslav Rudenko <slavezzz@inbox.ru>
2020-03-11 17:02:09 +0300

    Списки

--- snake/Program.cs
+++ snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -11,7 +12,29 @@ namespace snake
 
             Point p2 = new Point(4,5,'#');       
             p2.Draw();
-         
+
+            List<int> numList = new List<int>();
+
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
 
 
